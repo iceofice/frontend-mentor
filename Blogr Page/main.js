cf5f87d354a6153cc5b87b9b6b7c8570c3ac11af
iceofice <renireas@gmail.com>
2021-10-30 00:56:56 +0700

    Finish Blogr Page challenge

---
+++ Blogr Page/main.js
@@ -0,0 +1,61 @@
+var hamburger = document.getElementById("hamburger");
+var close = document.getElementById("close");
+var menu = document.getElementById("menu");
+menu.style.display = "none";
+
+hamburger.addEventListener("click", function () {
+  hamburger.style.display = "none";
+  close.style.display = "block";
+  menu.style.display = "block";
+});
+
+close.addEventListener("click", function () {
+  hamburger.style.display = "block";
+  close.style.display = "none";
+  menu.style.display = "none";
+});
+
+window.onscroll = function () {
+  stickyHeader();
+};
+
+var header = document.getElementsByTagName("header")[0];
+
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset >= sticky) {
+    console.log(sticky);
+    header.style.position = "fixed";
+    header.style.margin = "0 auto";
+    header.style.background = "hsla(9, 100%, 71%, 90%)";
+  } else {
+    header.style.position = "absolute";
+    header.style.margin = "3.5em auto";
+    header.style.background = "none";
+  }
+}
+
+var menuTitleElements = document.querySelectorAll(".mobile-menu .menu-title");
+var submenuElements = document.querySelectorAll(".mobile-menu .submenu");
+
+Array.from(menuTitleElements).map((menu, index) => {
+  menu.addEventListener("click", () => {
+    if (submenuElements[index].classList.contains("active")) {
+      submenuElements[index].classList.remove("active");
+      menu.classList.remove("active");
+      return;
+    }
+
+    Array.from(submenuElements).map((submenu) => {
+      submenu.classList.remove("active");
+    });
+
+    Array.from(menuTitleElements).map((menu) => {
+      menu.classList.remove("active");
+    });
+
+    submenuElements[index].classList.add("active");
+    menu.classList.add("active");
+  });
+});
